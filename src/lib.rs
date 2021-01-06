9c6fcf3ed8a94ac4a0601d5d19c27f0915e2ff00
Signet Mob <signet.mob@gmail.com>
2021-01-05 17:26:26 -0800

    Make the search function work correctly.

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,15 @@ pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec!["safe, fast, productive."]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
